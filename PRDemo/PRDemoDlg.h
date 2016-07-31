5a2d20f2a3380f98a4803b04dd7189385b91885d
Kyo <Kyo@KYO-PC>
2016-07-31 15:43:27 +0800

    添加项目文件。

---
+++ PRDemo/PRDemoDlg.h
@@ -0,0 +1,34 @@
+
+// PRDemoDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CPRDemoDlg 对话框
+class CPRDemoDlg : public CDialogEx
+{
+// 构造
+public:
+	CPRDemoDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PRDEMO_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
